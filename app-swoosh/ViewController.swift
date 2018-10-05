e8377189e7d0ad6b89f4021415664333ed14ddd5
Wellington Aquino <wellington.aquino@one.verizon.com>
2018-10-05 14:59:31 -0400

    Adding auto-layout constraints to views

--- app-swoosh/ViewController.swift
+++ app-swoosh/ViewController.swift
@@ -16,10 +16,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        swoosh.frame = CGRect(x: view.frame.size.width / 2 - swoosh.frame.size.width / 2, y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height)
-        
-        bgImage.frame = view.frame
+
     }
 
     override func didReceiveMemoryWarning() {
