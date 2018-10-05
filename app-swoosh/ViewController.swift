132b188bbaabfb939f30ea327084b894daedddc1
Wellington Aquino <wellington.aquino@one.verizon.com>
2018-10-05 12:03:09 -0400

    Adding view programmatically to reach pixel perfect apps

--- app-swoosh/ViewController.swift
+++ app-swoosh/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var bgImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        swoosh.frame = CGRect(x: view.frame.size.width / 2 - swoosh.frame.size.width / 2, y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height)
+        
+        bgImage.frame = view.frame
     }
 
     override func didReceiveMemoryWarning() {
