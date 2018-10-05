951dd9a3e2c82a94f34f583bb1c79b4e1ba77c35
Wellington Aquino <wellington.aquino@one.verizon.com>
2018-10-05 07:32:48 -0400

    Initial Commit

---
+++ app-swoosh/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Development Verizon on 10/5/18.
+//  Copyright © 2018 Development Verizon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
