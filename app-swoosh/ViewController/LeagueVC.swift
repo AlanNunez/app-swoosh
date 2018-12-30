a626628df7057d4df791425559880c0cdb492847
Alan Nunez <cocoadev@Alans-MacBook-Pro.local>
2018-12-30 19:00:16 -0400

    Adding Segues Programmatically

--- app-swoosh/ViewController/LeagueVC.swift
+++ app-swoosh/ViewController/LeagueVC.swift
@@ -13,4 +13,7 @@ class LeagueVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    @IBAction func onNextTapped(_ sender: Any) {
+        performSegue(withIdentifier: "skillVCSegue", sender: self)
+    }
 }
