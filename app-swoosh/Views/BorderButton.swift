8f570e9352e8d85ff59a57b707728ff2dd6f7c50
Alan Nunez <cocoadev@Alans-MacBook-Pro.local>
2018-12-29 21:50:08 -0400

    Default layout and custom buttons

---
+++ app-swoosh/Views/BorderButton.swift
@@ -0,0 +1,21 @@
+//
+//  BorderButton.swift
+//  app-swoosh
+//
+//  Created by Alan Nunez on 12/29/18.
+//  Copyright © 2018 Alan Nunez. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
